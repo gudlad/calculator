be4d788e048566729209c8ab99432e82233da744
gudladd <19a31@sdmit.in>
2023-10-01 10:04:38 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [currentOperand, setCurrentOperand] = useState("");
+  const [previousOperand, setPreviousOperand] = useState("");
+  const [operation, setOperation] = useState("");
+
+  function handleCurrentOperand(value) {
+    setCurrentOperand((currentOperand) => currentOperand + value);
+  }
+  function handleDelete() {
+    setCurrentOperand((currentOperand) =>
+      currentOperand.substring(0, currentOperand.length - 1)
+    );
+  }
+  function handleDeleteAll() {
+    setCurrentOperand("");
+  }
+
+  function validate() {
+    const regex = /^\d+\.?\d*$/;
+    if (!regex.test(currentOperand)) {
+      return false;
+    }
+    return true;
+  }
+
+  function handleOperatorSelection(value) {
+    if (validate()) {
+      setOperation(value);
+      setPreviousOperand(currentOperand);
+      setCurrentOperand("");
+    }
+  }
+
+  function handleSolution() {
+    if (!currentOperand || !previousOperand || !operation || !validate())
+      return;
+    const result = evaluate(previousOperand, operation, currentOperand);
+    setPreviousOperand(
+      (previousOperand) => previousOperand + operation + currentOperand
+    );
+    setCurrentOperand("" + result);
+    setOperation("");
+  }
+
+  function evaluate(previousOperand, operation, currentOperand) {
+    switch (operation) {
+      case "+":
+        return parseFloat(previousOperand) + parseFloat(currentOperand);
+      case "-":
+        return parseFloat(previousOperand) - parseFloat(currentOperand);
+      case "*":
+        return parseFloat(previousOperand) * parseFloat(currentOperand);
+      case "÷":
+        return parseFloat(previousOperand) / parseFloat(currentOperand);
+      default:
+        return 0;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calci--container">
+      <div className="calculator--grid">
+        <div className="output">
+          <div className="previous--operand">{previousOperand + operation}</div>
+          <div className="current--operand">{currentOperand}</div>
+        </div>
+        <button className="span--two" onClick={handleDeleteAll}>
+          AC
+        </button>
+        <button onClick={handleDelete}>DEL</button>
+        <button onClick={() => handleOperatorSelection("÷")}>÷</button>
+        <button onClick={() => handleCurrentOperand("1")}>1</button>
+        <button onClick={() => handleCurrentOperand("2")}>2</button>
+        <button onClick={() => handleCurrentOperand("3")}>3</button>
+        <button onClick={() => handleOperatorSelection("*")}>*</button>
+        <button onClick={() => handleCurrentOperand("4")}>4</button>
+        <button onClick={() => handleCurrentOperand("5")}>5</button>
+        <button onClick={() => handleCurrentOperand("6")}>6</button>
+        <button onClick={() => handleOperatorSelection("+")}>+</button>
+        <button onClick={() => handleCurrentOperand("7")}>7</button>
+        <button onClick={() => handleCurrentOperand("8")}>8</button>
+        <button onClick={() => handleCurrentOperand("9")}>9</button>
+        <button onClick={() => handleOperatorSelection("-")}>-</button>
+        <button onClick={() => handleCurrentOperand(".")}>.</button>
+        <button onClick={() => handleCurrentOperand("0")}>0</button>
+        <button className="span--two" onClick={handleSolution}>
+          =
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
